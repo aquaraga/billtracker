c6e233091141fa2e5d07f99744b63f4323c6a307
Kumar <iyer.kumar83@gmail.com>
2013-04-20 00:09:51 +0530

    Abstracting the db interaction into a separate class, in a bid to move it out of the controller

---
+++ BillTracker/Services/BillService.cs
@@ -0,0 +1,20 @@
+﻿using BillTracker.Models;
+
+namespace BillTracker.Services
+{
+    public class BillService : IBillService
+    {
+        private readonly IBillContext billContext;
+
+        public BillService(IBillContext billContext)
+        {
+            this.billContext = billContext;
+        }
+
+        public void SaveBill(BillModel billModel)
+        {
+            billContext.Bills.Add(billModel);
+            billContext.SaveChanges();
+        }
+    }
+}
---
+++ BillTracker/Services/IBillService.cs
@@ -0,0 +1,9 @@
+﻿using BillTracker.Models;
+
+namespace BillTracker.Services
+{
+    public interface IBillService
+    {
+        void SaveBill(BillModel billModel);
+    }
+}
